966cd96241ccd9487819e022464e912a8bb131b6
akiyu33333 <aaaaaki35@gmail.com>
2019-08-16 21:27:15 +0900

    棒倒し法で実装

--- src/main/java/maze/maker/Maker.java
+++ src/main/java/maze/maker/Maker.java
@@ -1,19 +1,21 @@
 package maze.maker;
 
+import java.util.Objects;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Maker {
     private static final String WALL = "■";
     private static final String ROAD = "　";
 
-
     String[][] board;
 
-    public Maker(int x , int y) {
+    public Maker(int x, int y) {
         this.board = new String[y][x];
     }
 
     public void make() {
+        //外壁と1マス飛ばしで内壁を作成
         for (int i = 0; i < board.length; i++) {
             int ix = i;
             if (i == 0 || i == (board.length - 1)) {
@@ -30,6 +32,104 @@ public class Maker {
                 board[ix][iy] = iy % 2 == 0 ? WALL : ROAD;
             }
         }
+
+        Random rand = new Random();
+        //棒倒し法で作成
+        for (int i = 2; i < board.length; i = i + 2) {
+            for (int j = 2; j < board.length; j = j + 2) {
+                String point = board[i][j];
+                if (Objects.equals(point, ROAD)) {
+                    continue;
+                }
+                if (i == 2) {
+                    //壁がある場所には倒せない
+                    boolean flg = false;
+                    while (!flg) {
+                        //4方向にランダムで壁を作成
+                        int r = rand.nextInt(4);
+                        try {
+                            switch (r) {
+                                case 0:
+                                    String p = board[i - 1][j];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i - 1][j] = WALL;
+                                    flg = true;
+                                    break;
+                                case 1:
+                                    p = board[i][j - 1];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i][j - 1] = WALL;
+                                    flg = true;
+                                    break;
+                                case 2:
+                                    p = board[i + 1][j];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i + 1][j] = WALL;
+                                    flg = true;
+                                    break;
+                                default:
+                                    p = board[i][j + 1];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i][j + 1] = WALL;
+                                    flg = true;
+                                    break;
+                            }
+                        } catch (Throwable t) {
+                            break;
+                        }
+                    }
+                } else {
+                    //3方向にランダムで壁を作成
+                    //壁がある場所には倒せない
+                    boolean flg = false;
+                    while (!flg) {
+                        //4方向にランダムで壁を作成
+                        int r = rand.nextInt(3);
+                        try {
+                            switch (r) {
+                                case 0:
+                                    String p = board[i][j - 1];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i][j - 1] = WALL;
+                                    flg = true;
+                                    break;
+                                case 1:
+                                    p = board[i + 1][j];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i + 1][j] = WALL;
+                                    flg = true;
+                                    break;
+                                default:
+                                    p = board[i][j + 1];
+                                    if (Objects.equals(p, WALL)) {
+                                        continue;
+                                    }
+                                    board[i][j + 1] = WALL;
+                                    flg = true;
+                                    break;
+                            }
+                        } catch (Throwable t) {
+                            break;
+                        }
+                    }
+                }
+            }
+            board[0][1] = "Ｓ";
+            board[board.length - 1][board[board.length - 1].length - 2] = "Ｇ";
+        }
+
     }
 
     public void print() {
