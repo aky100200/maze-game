4d65196161f0e00ada6713c3da2a8f11ec7270b5
akiyu33333 <aaaaaki35@gmail.com>
2019-08-17 21:40:58 +0900

    リファクタリング

--- src/main/java/maze/game/SolveMaze.java
+++ src/main/java/maze/game/SolveMaze.java
@@ -1,7 +1,6 @@
 package maze.game;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import maze.obj.Board;
 import maze.obj.Cells;
 
@@ -9,43 +8,43 @@ import java.util.LinkedList;
 
 public class SolveMaze {
     public Board solve(Board board) {
-        //開始位置 0,1
-        int x = 0;
-        int y = 1;
+        //開始位置
+        Point startCell = findStartCell(board);
+        int x = startCell.x;
+        int y = startCell.y;
 
-        //行ったことがある場合はtrue
-        boolean[][] been = new boolean[21][21];
+        //通過した座標をtrueにする
+        boolean[][] been = new boolean[board.getXLength()][board.getYLength()];
 
+        //ルート記憶のためのスタック
         LinkedList<Point> stack = new LinkedList<>();
         been[x][y] = true;
         stack.push(new Point(x, y));
 
-        //4方向
-        //上下左右が壁か確認
         while (board.getCell(x, y) != Cells.GOAL) {
             //上
-            if (x > 0 && (board.getCell(x - 1, y) == Cells.ROAD || board.getCell(x - 1, y) == Cells.GOAL) && !been[x - 1][y]) {
+            if (x > 0 && isValidCell(x - 1, y, board, been)) {
                 x = x - 1;
                 been[x][y] = true;
                 stack.push(new Point(x, y));
                 continue;
             }
             //左
-            else if (y > 0 && (board.getCell(x, y - 1) == Cells.ROAD || board.getCell(x, y - 1) == Cells.GOAL) && !been[x][y - 1]) {
+            else if (y > 0 && isValidCell(x, y - 1, board, been)) {
                 y = y - 1;
                 been[x][y] = true;
                 stack.push(new Point(x, y));
                 continue;
             }
             //右
-            else if (board.getXLength() > x + 1 && (board.getCell(x + 1, y) == Cells.ROAD || board.getCell(x + 1, y) == Cells.GOAL) && !been[x + 1][y]) {
+            else if (board.getXLength() > x + 1 && isValidCell(x + 1, y, board, been)) {
                 x = x + 1;
                 been[x][y] = true;
                 stack.push(new Point(x, y));
                 continue;
             }
             //下
-            else if (board.getYLength() > y + 1 && (board.getCell(x, y + 1) == Cells.ROAD || board.getCell(x, y + 1) == Cells.GOAL) && !been[x][y + 1]) {
+            else if (board.getYLength() > y + 1 && isValidCell(x, y + 1, board, been)) {
                 y = y + 1;
                 been[x][y] = true;
                 stack.push(new Point(x, y));
@@ -57,11 +56,10 @@ public class SolveMaze {
                 stack.poll();
                 //さらに1つ前を参照
                 Point p = stack.peek();
-                x = p.getX();
-                y = p.getY();
+                x = p.x;
+                y = p.y;
             }
         }
-        //end
         //キューに入っている座標の場所を置換してルートを示す
         while (stack.size() != 0) {
             Point p = stack.poll();
@@ -70,9 +68,22 @@ public class SolveMaze {
         return board;
     }
 
+    private Point findStartCell(Board board) {
+        for (int i = 0; i < board.getXLength(); i++) {
+            for (int j = 0; j < board.getYLength(); j++) {
+                if (board.getCell(i, j) == Cells.START) return new Point(i, j);
+            }
+        }
+        throw new RuntimeException("Start Cell Not Found.");
+    }
+
+
+    private boolean isValidCell(int x, int y, Board board, boolean[][] been) {
+        return (board.getCell(x, y) == Cells.ROAD || board.getCell(x, y) == Cells.GOAL) && !been[x][y];
+    }
+
     @AllArgsConstructor
-    @Data
-    public static class Point{
+    private static class Point {
         int x;
         int y;
     }
